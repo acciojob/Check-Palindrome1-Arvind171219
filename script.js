c942049229371abc51c2b37d1eeccdb5704e94d3
Arvind kumar <74968170+Arvind171219@users.noreply.github.com>
2023-06-13 18:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(s){
+const sanitizedString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  // Compare the reversed string with the original string
+  const reversedString = sanitizedString.split('').reverse().join('');
+  return sanitizedString === reversedString;
 }
 module.exports = palindrome
